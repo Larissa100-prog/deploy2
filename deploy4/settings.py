7edf70da576e20a563e9e5c3fa9f8cc348797d09
Larissa Andrade Savoiardo Lisboa <1242116408@acd.grupotiradentes.com>
2024-11-23 10:45:49 -0300

    deploy2

--- deploy4/settings.py
+++ deploy4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9f$a_gcd1df*w)$yn6m8*xfq!exs)r4el3(1qf1vrxvfl=20=5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
